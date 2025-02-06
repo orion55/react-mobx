4b0837a7b13c843a51bdb5909c65e6697850f2cc
Grebenev Oleg Evgenevich <OGrebenev@nota.tech>
2025-02-06 16:35:47 +0500

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/react-mobx/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
 });
